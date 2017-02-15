eacecdc08e362c54db0df760050c03cb895ea8c0
darlenedilla <darlenedilla@hotmail.com>
2017-02-15 11:25:58 +0100

    Project JQuery
    hej

--- ProjectScript.js
+++ ProjectScript.js
@@ -1,34 +1,26 @@
-function findProjects() {
+/*function findProjects() {
     var req = new XMLHttpRequest();
     req.addEventListener("load", showProjects);
     req.open("GET", "http://www.centisoft.dk/api/Project");
     req.send();
 }
+*/
 
-function findProjectOnID() {
-    var oReq = new XMLHttpRequest();
-    var projID = document.getElementById("idField").value;
-    oReq.addEventListener("load", showProjects(projID));
-    oReq.open("GET", "http://www.centisoft.dk/api/Project/?Id="+ projID);
-    oReq.send();
 
-}
-
-function showProjects(id) {
-    var projectList = JSON.parse(this.responseText);
+function showProjects(data) {
+    //var projectList = JSON.parse(this.responseText);
     var element = document.getElementById("ProjectBody");
     element.innerHTML ="";
-    for(var i = 0; i < projectList.length; i++) {
-    //if(projectList[i].Id === id || id === null) {
+    for(var i = 0; i < data.length; i++) {
             var rowElement = document.createElement("tr");
             var firstColumn = document.createElement("td");
             var secondColumn = document.createElement("td");
             var thirdColumn = document.createElement("td");
             var fourthColumn = document.createElement("td");
-            var firstColumnText = document.createTextNode(projectList[i].Id);
-            var secondColumnText = document.createTextNode(projectList[i].Name);
-            var thirdColumnText = document.createTextNode(projectList[i].DueDate);
-            var fourthColumnText = document.createTextNode(projectList[i].CustomerId);
+            var firstColumnText = document.createTextNode(data[i].Id);
+            var secondColumnText = document.createTextNode(data[i].Name);
+            var thirdColumnText = document.createTextNode(data[i].DueDate);
+            var fourthColumnText = document.createTextNode(data[i].CustomerId);
 
             rowElement.appendChild(firstColumn);
             rowElement.appendChild(secondColumn);
@@ -41,18 +33,33 @@ function showProjects(id) {
             fourthColumn.appendChild(fourthColumnText);
             
             element.appendChild(rowElement);
-        }
-    //}
+    }
 }
 
-function InsertProject() {
-    var oReq = new XMLHttpRequest();
-    var parameters = {"Id": document.getElementById("id"), "Name": document.getElementById("name"),"DueDate": document.getElementById("dueDate"), "CustomerId": document.getElementById("customerID") };
-    var jsonParam = JSON.stringify(parameters);
-    oReq.addEventListener("load", updatemessageProject);
-    oReq.open("POST", "http://centisoft.dk/api/Project/");
-    oReq.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    oReq.send(jsonParam);
+function showOneProject(data){
+    var element = document.getElementById("ProjectBody");
+    element.innerHTML ="";
+            var rowElement = document.createElement("tr");
+            var firstColumn = document.createElement("td");
+            var secondColumn = document.createElement("td");
+            var thirdColumn = document.createElement("td");
+            var fourthColumn = document.createElement("td");
+            var firstColumnText = document.createTextNode(data.Id);
+            var secondColumnText = document.createTextNode(data.Name);
+            var thirdColumnText = document.createTextNode(data.DueDate);
+            var fourthColumnText = document.createTextNode(data.CustomerId);
+
+            rowElement.appendChild(firstColumn);
+            rowElement.appendChild(secondColumn);
+            rowElement.appendChild(thirdColumn);
+            rowElement.appendChild(fourthColumn);
+
+            firstColumn.appendChild(firstColumnText);
+            secondColumn.appendChild(secondColumnText);
+            thirdColumn.appendChild(thirdColumnText);
+            fourthColumn.appendChild(fourthColumnText);
+            
+            element.appendChild(rowElement);
 }
 
 
@@ -60,15 +67,6 @@ function updatemessageProject() {
     alert("detvirker");
 }
 
-function updateProject() {
-    var oReq = new XMLHttpRequest();
-    var parameters = {"Id": document.getElementById("id"), "Name": document.getElementById("name"),"DueDate": document.getElementById("dueDate"), "CustomerId": document.getElementById("customerID") };
-    var jsonParam = JSON.stringify(parameters);
-    oReq.addEventListener("load", updatemessageProject);
-    oReq.open("PUT", "http://centisoft.dk/api/Project/");
-    oReq.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    oReq.send(jsonParam);
-}
 
 function DeleteProject(){
     var oReq = new XMLHttpRequest();

539fa334efa32498c9576be98c690cb7e0c6c35a
darlenedilla <darlenedilla@hotmail.com>
2017-02-20 12:34:52 +0100

    det virker ikke

--- ProjectScript.js
+++ ProjectScript.js
@@ -62,6 +62,40 @@ function showOneProject(data){
             element.appendChild(rowElement);
 }
 
+function showTaskById(data){
+    var element = document.getElementById("taskBody");
+    element.innerHTML ="";
+            var rowElement = document.createElement("tr");
+            var firstColumn = document.createElement("td");
+            var secondColumn = document.createElement("td");
+            var thirdColumn = document.createElement("td");
+            var fourthColumn = document.createElement("td");
+            var fifthColumn = document.createElement("td");
+            var sixthColumn = document.createElement("td");
+            var firstColumnText = document.createTextNode(data.Id);
+            var secondColumnText = document.createTextNode(data.Name);
+            var thirdColumnText = document.createTextNode(data.Description);
+            var fourthColumnText = document.createTextNode(data.Created);
+            var fifthColumnText = document.createTextNode(data.Duration);
+            var sixthColumnText = document.createTextNode(data.ProjectId);
+
+            rowElement.appendChild(firstColumn);
+            rowElement.appendChild(secondColumn);
+            rowElement.appendChild(thirdColumn);
+            rowElement.appendChild(fourthColumn);
+            rowElement.appendChild(fifthColumn);
+            rowElement.appendChild(sixthColumn);
+
+            firstColumn.appendChild(firstColumnText);
+            secondColumn.appendChild(secondColumnText);
+            thirdColumn.appendChild(thirdColumnText);
+            fourthColumn.appendChild(fourthColumnText);
+            fifthColumn.appendChild(fifthColumnText);
+            sixthColumn.appendChild(sixthColumnText);
+            
+            element.appendChild(rowElement);
+}
+
 
 function updatemessageProject() {
     alert("detvirker");
